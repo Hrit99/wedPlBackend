c31033e08d1d0d850031b4dfcde65d57e40fb26f
Hrit99 <hritikpihal51299@gmail.com>
2021-04-11 01:30:21 +0530

    added image uploading apis

--- routes/itemsManageRoute.js
+++ routes/itemsManageRoute.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const imgur = require('imgur');
 
 const itemsController = require('../controller/itemsController')
 var itemModel = require('../model/items').item
@@ -13,13 +14,29 @@ router.post('/', (req, res) => {
 router.post('/storeItem', (req, res) => {
     console.log(req.body.category)
     console.log(req.body.subcategory)
-    itemsController.storeItem(itemModel(req),res, req.body.name, req.body.mediaUrl, req.body.details)
+    imgur
+  .uploadFile(req.body.path)
+  .then((json) => {
+    itemsController.storeItem(itemModel(req),res, req.body.name, json.link, req.body.details)
+  })
+  .catch((err) => {
+    console.error(err.message);
+  });
+    
 })
 
 router.post('/updateItem', (req, res) => {
     console.log(req.body.category)
     console.log(req.body.subcategory)
-    itemsController.updateItem(itemModel(req),res, req.body._id, req.body.name, req.body.mediaUrl, req.body.details)
+    imgur
+    .uploadFile(req.body.path)
+    .then((json) => {
+      itemsController.updateItem(itemModel(req),res, req.body._id, req.body.name, json.link, req.body.details)
+    })
+    .catch((err) => {
+      console.error(err.message);
+    });
+   
 })
 
 router.post('/deleteItem', (req, res) => {
