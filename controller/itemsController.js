b37c020cdb2d823200edff22838db1c3bfe9eadc
Hrit99 <hritikpihal51299@gmail.com>
2021-04-10 13:21:32 +0530

    added update and delete item apis

--- controller/itemsController.js
+++ controller/itemsController.js
@@ -47,6 +47,47 @@ function storeItem(item,res,name, mediaUrl, details){
     )
 }
 
+
+function updateItem(item, res, _id, name, mediaUrl, details){
+    let itemId = _id
+
+    let updatedData = {
+        name: name,
+        mediaUrl: mediaUrl,
+        details: details,
+    }
+
+    item.findByIdAndUpdate(itemId, {$set: updatedData})
+    .then(() => {
+        res.json({
+            updated: true
+        })
+    })
+    .catch(error => {
+        res.json({
+            updated: false
+        })
+    })
+}
+
+function destroyItem(item, res, _id){
+    let itemId = _id
+    console.log(itemId)
+    item.findByIdAndRemove(itemId)
+    .then(() => {
+        res.json({
+            deleted: true
+        })
+    })
+    .catch(error => {
+        res.json({
+            deleted: false
+        })
+    })
+    
+}
+
+
 module.exports = {
-    showItem, storeItem
+    showItem, storeItem, updateItem, destroyItem
 }
--- routes/itemsManageRoute.js
+++ routes/itemsManageRoute.js
@@ -3,13 +3,9 @@ const router = express.Router()
 
 const itemsController = require('../controller/itemsController')
 var itemModel = require('../model/items').item
-let category
-let subcategory
 router.post('/', (req, res) => {
     console.log(req.body.category)
     console.log(req.body.subcategory)
-    category = req.body.category
-    subcategory = req.body.subcategory
     itemsController.showItem(itemModel(req), res)
 }
 )
@@ -17,10 +13,19 @@ router.post('/', (req, res) => {
 router.post('/storeItem', (req, res) => {
     console.log(req.body.category)
     console.log(req.body.subcategory)
-    category = req.body.category
-    subcategory = req.body.subcategory
-    inpreq = req
     itemsController.storeItem(itemModel(req),res, req.body.name, req.body.mediaUrl, req.body.details)
+})
+
+router.post('/updateItem', (req, res) => {
+    console.log(req.body.category)
+    console.log(req.body.subcategory)
+    itemsController.updateItem(itemModel(req),res, req.body._id, req.body.name, req.body.mediaUrl, req.body.details)
+})
+
+router.post('/deleteItem', (req, res) => {
+    console.log(req.body.category)
+    console.log(req.body.subcategory)
+    itemsController.destroyItem(itemModel(req),res, req.body._id)
 })
 
 module.exports = {
